c3a5a64742190184d908175536142aac9becab82
silverhack <diariode@gmail.com>
2025-06-25 11:05:50 +0200

    update tenant info logic

--- core/tenant/Get-TenantInformation.ps1
+++ core/tenant/Get-TenantInformation.ps1
@@ -35,133 +35,122 @@ Function Get-TenantInformation{
     #>
     [CmdletBinding()]
     Param()
-    #Create hashtable
-    $tenantInfo= [PsCustomObject]@{
-        tenantName = $null;
-        tenantId = $null;
-        companyInfo = $null;
-        sku = $null;
-        domains = $null;
-        myDomain = $null;
-        licensing = $null;
-    }
-    If($O365Object.auth_tokens.MSGraph -and $O365Object.TenantId){
+    Process{
         Try{
-            #Write message
-            $msg = @{
-                MessageData = ($message.AADTenantInfoMessage -f $O365Object.TenantId);
-                callStack = (Get-PSCallStack | Select-Object -First 1);
-                logLevel = 'verbose';
-                Tags = @('AADTenantInfo');
-            }
-            Write-Verbose @msg
-            #Get Auth from old graph
-            $msgraph_auth = $O365Object.auth_tokens.MSGraph
-            #Get Tenant Origin
-            If($O365Object.isValidTenantGuid -eq $false){
-                $tid = Read-JWTtoken -token $O365Object.auth_tokens.MSGraph.AccessToken | Select-Object -ExpandProperty tid -ErrorAction Ignore
-            }
-            Else{
-                $tid = $O365Object.TenantId
-            }
-            #Get tenant details
-            $Tenant = Get-MonkeyMSGraphOrganization -TenantId $tid
-            If($Tenant){
-                #Set Tenant info var
-                Set-Variable Tenant -Value $Tenant -Scope Script -Force
-                If($O365Object.isConfidentialApp){
-                    #Set Userprincipalname var
-                    If($msgraph_auth.psobject.Properties.Item('clientId')){
-                        Set-Variable userPrincipalName -Value $msgraph_auth.clientId.ToString() -Scope Script -Force
-                        $O365Object.userPrincipalName = $msgraph_auth.clientId.ToString()
+            If($O365Object.auth_tokens.MSGraph -and $O365Object.TenantId){
+                #Write message
+                $msg = @{
+                    MessageData = ($message.AADTenantInfoMessage -f $O365Object.TenantId);
+                    callStack = (Get-PSCallStack | Select-Object -First 1);
+                    logLevel = 'verbose';
+                    Tags = @('EIDTenantInfo');
+                }
+                Write-Verbose @msg
+                #Get Tenant Origin
+                If($O365Object.isValidTenantGuid -eq $false){
+                    $tid = Read-JWTtoken -token $O365Object.auth_tokens.MSGraph.AccessToken | Select-Object -ExpandProperty tid -ErrorAction Ignore
+                }
+                Else{
+                    $tid = $O365Object.TenantId
+                }
+                #Get tenant details
+                $Tenant = Get-MonkeyMSGraphOrganization -TenantId $tid
+                If($Tenant){
+                    #Set Tenant info var
+                    Set-Variable Tenant -Value $Tenant -Scope Script -Force
+                    If($O365Object.isConfidentialApp){
+                        #Set Userprincipalname var
+                        If($O365Object.auth_tokens.MSGraph.psobject.Properties.Item('clientId')){
+                            Set-Variable userPrincipalName -Value $O365Object.auth_tokens.MSGraph.clientId.ToString() -Scope Script -Force
+                            $O365Object.userPrincipalName = $O365Object.auth_tokens.MSGraph.clientId.ToString()
+                        }
+                        Else{
+                            $msg = @{
+                                MessageData = $message.AADUserErrorMessage;
+                                callStack = (Get-PSCallStack | Select-Object -First 1);
+                                logLevel = 'warning';
+                                Tags = @('EIDUserPrincipalNameError');
+                            }
+                            Write-Warning @msg
+                            Set-Variable userPrincipalName -Value $null -Scope Script -Force
+                            $O365Object.userPrincipalName = $O365Object.initParams.ClientId
+                        }
                     }
                     Else{
-                        $msg = @{
-                            MessageData = $message.AADUserErrorMessage;
-                            callStack = (Get-PSCallStack | Select-Object -First 1);
-                            logLevel = 'warning';
-                            Tags = @('AADUserPrincipalNameError');
+                        #Set Userprincipalname var
+                        If($O365Object.auth_tokens.MSGraph.psobject.Properties.Item('UserInfo')){
+                            $O365Object.userPrincipalName = $O365Object.auth_tokens.MSGraph.UserInfo.DisplayableId.ToString()
+                            Set-Variable userPrincipalName -Value $O365Object.auth_tokens.MSGraph.UserInfo.DisplayableId.ToString() -Scope Script -Force
                         }
-                        Write-Warning @msg
-                        Set-Variable userPrincipalName -Value $null -Scope Script -Force
-                        $O365Object.userPrincipalName = $O365Object.initParams.ClientId
+                        ElseIf($O365Object.auth_tokens.MSGraph.psobject.Properties.Item('userPrincipalName')){
+                            $O365Object.userPrincipalName = $O365Object.auth_tokens.MSGraph.userPrincipalName
+                            Set-Variable userPrincipalName -Value $O365Object.auth_tokens.MSGraph.userPrincipalName -Scope Script -Force
+                        }
+                        ElseIf($O365Object.auth_tokens.MSGraph.psobject.Properties.Item('Account')){
+                            $O365Object.userPrincipalName = $O365Object.auth_tokens.MSGraph.Account.Username
+                            Set-Variable userPrincipalName -Value $O365Object.auth_tokens.MSGraph.Account.Username -Scope Script -Force
+                        }
+                        Else{
+                            $msg = @{
+                                MessageData = $message.AADUserErrorMessage;
+                                callStack = (Get-PSCallStack | Select-Object -First 1);
+                                logLevel = 'warning';
+                                Tags = @('EIDUserPrincipalNameError');
+                            }
+                            Write-Warning @msg
+                            Set-Variable userPrincipalName -Value $null -Scope Script -Force
+                            $O365Object.userPrincipalName = $null
+                        }
+                    }
+                    #Set properties
+                    $O365Object.Tenant.tenantName = $Tenant.displayName
+                    $O365Object.Tenant.companyInfo = $Tenant
+                    $O365Object.Tenant.tenantId = $Tenant.Id
+                    #Get subscribed SKUs
+                    $O365Object.Tenant.sku = Get-MonkeyMSGraphSuscribedSku
+                    If($null -ne $O365Object.Tenant.sku){
+                        #Get licensing info from current tenant
+                        $O365Object.Tenant.licensing = Get-TenantLicensingInfo -SKU $O365Object.Tenant.sku
+                    }
+                    #Get Domains
+                    $O365Object.Tenant.domains = Get-MonkeyMSGraphDomain
+                    If($null -ne $O365Object.Tenant.domains){
+                        #Set property
+                        $O365Object.Tenant.myDomain = $O365Object.Tenant.domains | Where-Object {$_.IsDefault -eq $true}
                     }
                 }
                 Else{
-                    #Set Userprincipalname var
-                    If($msgraph_auth.psobject.Properties.Item('UserInfo')){
-                        $O365Object.userPrincipalName = $msgraph_auth.UserInfo.DisplayableId.ToString()
-                        Set-Variable userPrincipalName -Value $msgraph_auth.UserInfo.DisplayableId.ToString() -Scope Script -Force
-                    }
-                    ElseIf($msgraph_auth.psobject.Properties.Item('userPrincipalName')){
-                        $O365Object.userPrincipalName = $msgraph_auth.userPrincipalName
-                        Set-Variable userPrincipalName -Value $msgraph_auth.userPrincipalName -Scope Script -Force
-                    }
-                    ElseIf($msgraph_auth.psobject.Properties.Item('Account')){
-                        $O365Object.userPrincipalName = $msgraph_auth.Account.Username
-                        Set-Variable userPrincipalName -Value $msgraph_auth.Account.Username -Scope Script -Force
-                    }
-                    Else{
-                        $msg = @{
-                            MessageData = $message.AADUserErrorMessage;
-                            callStack = (Get-PSCallStack | Select-Object -First 1);
-                            logLevel = 'warning';
-                            Tags = @('AADUserPrincipalNameError');
-                        }
-                        Write-Warning @msg
-                        Set-Variable userPrincipalName -Value $null -Scope Script -Force
-                        $O365Object.userPrincipalName = $null
-                    }
+                    #Throw error
+                    throw ("[TenantError] {0}: {1}" -f "Unable to get tenant information",$_.Exception.Message)
                 }
-                #Set properties
-                $tenantInfo.tenantName = $Tenant.displayName
-                $tenantInfo.companyInfo = $Tenant
-                $tenantInfo.tenantId = $Tenant.Id
             }
-            #Get subscribed SKUs
-            $SKus = Get-MonkeyMSGraphSuscribedSku
-            If($SKus){
-                #Set property
-                $tenantInfo.sku = $SKus
-                #Get licensing info from current tenant
-                $licensingInfo = Get-TenantLicensingInfo -SKU $SKus
-                #Set property
-                $tenantInfo.licensing = $licensingInfo;
+            Else{
+                $msg = @{
+                    MessageData = $message.O365TenantInfoError;
+                    callStack = (Get-PSCallStack | Select-Object -First 1);
+                    logLevel = 'warning';
+                    Tags = @('AADTenantError');
+                }
+                Write-Warning @msg
             }
-            #Get Domains
-            $tenantInfo.domains = Get-MonkeyMSGraphDomain
-            If($null -ne $tenantInfo.domains){
-                #Set property
-                $tenantInfo.myDomain = $tenantInfo.domains | Where-Object {$_.IsDefault -eq $true}
-            }
-            #return Obj
-            return $tenantInfo
         }
         Catch{
             $msg = @{
                 MessageData = $message.O365TenantInfoError;
                 callStack = (Get-PSCallStack | Select-Object -First 1);
                 logLevel = 'warning';
-                Tags = @('AADTenantError');
+                Tags = @('EIDTenantError');
             }
             Write-Warning @msg
             $msg = @{
                 MessageData = $_;
                 callStack = (Get-PSCallStack | Select-Object -First 1);
-                logLevel = 'debug';
-                Tags = @('AADTenantError');
+                logLevel = 'verbose';
+                Tags = @('EIDTenantError');
             }
-            Write-Debug @msg
+            Write-Verbose @msg
+            #Throw error
+            throw ("[TenantError] {0}: {1}" -f "Unable to get tenant information",$_.Exception.Message)
         }
-    }
-    Else{
-        $msg = @{
-            MessageData = $message.O365TenantInfoError;
-            callStack = (Get-PSCallStack | Select-Object -First 1);
-            logLevel = 'warning';
-            Tags = @('AADTenantError');
-        }
-        Write-Warning @msg
     }
 }
-
